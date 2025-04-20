e0322ae0a787c857f80404ff95c5f06a2f09d0a0
Shoaib Rashid <shoaib.lahore90@gmail.com>
2025-04-20 23:18:51 +0500

    allowed host

--- protoinv/protoinv/settings.py
+++ protoinv/protoinv/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_17^uk8xey-d12x=zanu9qi1*s$k1za(w%sxkf^gzb)7w)$5#4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.60.69.44']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
